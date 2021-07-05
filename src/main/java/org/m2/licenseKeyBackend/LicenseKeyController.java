d924ac584ac4850d3566b9e740be7ad45db194f6
niko <antonionikolai@gmail.com>
2021-07-05 09:50:36 +0200

    Final project with basic html interface to obtain or validate a license key

--- src/main/java/org/m2/licenseKeyBackend/LicenseKeyController.java
+++ src/main/java/org/m2/licenseKeyBackend/LicenseKeyController.java
@@ -4,6 +4,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.security.NoSuchAlgorithmException;
 
+/**
+ * @desc The controller for the api it is very basic and no input checks or sanitation are done in any way. The responses are also very basic and are just true and false for a given email address/key combination or a new key for a provided email address.
+ */
+
 @RestController
 public class LicenseKeyController {
 
--- src/main/java/org/m2/licenseKeyBackend/PageController.java
+++ src/main/java/org/m2/licenseKeyBackend/PageController.java
@@ -3,6 +3,10 @@ package org.m2.licenseKeyBackend;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * @desc The controller for the basic frontend providing a way to obtain a new license key or validate an existing on
+ */
+
 @Controller
     public class PageController {
 
